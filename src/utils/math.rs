7d21e57b0b3944b3e0fb354eefd0905de494ebeb
Lorenzo Panieri <panierilorenzo@gmail.com>
2021-02-16 00:29:43 +0100

    fixed functions not being public

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -1,10 +1,10 @@
 /// maps x such that: x in [a, b] -> y in [c, d]
-fn map_range(x: f32, x_min: f32, x_max: f32, y_min: f32, y_max: f32) -> f32 {
+pub fn map_range(x: f32, x_min: f32, x_max: f32, y_min: f32, y_max: f32) -> f32 {
     return (y_max - y_min) / (x_max - x_min) * (x - x_min) + y_min;
 }
 
 /// maps x such that: x in [a, b] -> y in [0, 1]
-fn map_normal(x: f32, x_min: f32, mut x_max: f32) -> f32 {
+pub fn map_normal(x: f32, x_min: f32, mut x_max: f32) -> f32 {
     x_max = x_max*0.999_999_9 + 0.000_000_1;  // prevent division by zero
     // precondition: x in [a, b]
     // debug_assert!(a <= x);
