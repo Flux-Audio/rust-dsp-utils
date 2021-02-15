1fd75de2a5d7ef92244f6b4e7076048eb3202aba
Lorenzo Panieri <panierilorenzo@gmail.com>
2021-02-15 20:14:26 +0100

    made modules work

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
-pub mod chaos;
+pub mod utils;
 
 /**
  *  Unit tests
  */
 #[cfg(test)]
 mod tests {
+    use rand_xoshiro::rand_core::SeedableRng;
+    use rand_xoshiro::Xoshiro256Plus;
+
+    use crate::utils::chaos;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_randf() {
+        let mut rng = Xoshiro256Plus::seed_from_u64(42635680);
+        for _i in 0..100000 {
+            let num = chaos::randf(&mut rng);
+            assert!(num >= 0.0 && num <= 1.0);
+        }
     }
 }
